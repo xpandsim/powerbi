b84df1ceb32edb3c437edc8934b6fb0ab01b0873
xpandsim <xpandsim@gmail.com>
2025-03-17 11:36:53 +0200

    Add files via upload

---
+++ en/story_content/triggers.js
@@ -0,0 +1,138 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5iKjd2WVYAM":
+        Script1();
+        break;
+      case "6TpvMAl2HLq":
+        Script2();
+        break;
+      case "5VdFRYZTU2M":
+        Script3();
+        break;
+      case "6IUCnicZtvT":
+        Script4();
+        break;
+      case "5ZxWPZ5PAxG":
+        Script5();
+        break;
+      case "6Wix6V9FkyS":
+        Script6();
+        break;
+      case "5iVQz3cholv":
+        Script7();
+        break;
+      case "5XtSnmHnzGT":
+        Script8();
+        break;
+      case "6MVaijAWKFz":
+        Script9();
+        break;
+      case "6iBTqRr98f2":
+        Script10();
+        break;
+      case "6VhHOyraaFb":
+        Script11();
+        break;
+      case "62XWvKoeFHa":
+        Script12();
+        break;
+      case "5WCNDUWHKY2":
+        Script13();
+        break;
+      case "5cCRgwAJceb":
+        Script14();
+        break;
+      case "627hGAdidMN":
+        Script15();
+        break;
+      case "62BYmZRfxUo":
+        Script16();
+        break;
+      case "5Zj4nrdR5cf":
+        Script17();
+        break;
+      case "62EU58IpRaJ":
+        Script18();
+        break;
+      case "6UQs9Cw3GOz":
+        Script19();
+        break;
+      case "5u9HADjOzvr":
+        Script20();
+        break;
+      case "6abA0Fc7lWC":
+        Script21();
+        break;
+      case "5rnzoR3Vxfd":
+        Script22();
+        break;
+      case "6Et6zEwFuUr":
+        Script23();
+        break;
+      case "61C3kpOjZGE":
+        Script24();
+        break;
+      case "6G9ZoBMGeNh":
+        Script25();
+        break;
+      case "6WrtTRSZ9aX":
+        Script26();
+        break;
+      case "5j6Au5prvfv":
+        Script27();
+        break;
+      case "6Qs7rlfMtsg":
+        Script28();
+        break;
+      case "6et5LnL5oYl":
+        Script29();
+        break;
+      case "6MaF8A9EEZ0":
+        Script30();
+        break;
+      case "6XOg4dhTVJO":
+        Script31();
+        break;
+      case "6aEKxpZe0AW":
+        Script32();
+        break;
+      case "6mVXZsfOaQB":
+        Script33();
+        break;
+      case "5cMSMIhPuip":
+        Script34();
+        break;
+      case "5XMZ5sljL74":
+        Script35();
+        break;
+      case "5ccvCfVsNrg":
+        Script36();
+        break;
+      case "63xbC7DuAYI":
+        Script37();
+        break;
+      case "64RZdp76KjJ":
+        Script38();
+        break;
+      case "6OgfRds8hfg":
+        Script39();
+        break;
+      case "6MCIXaTvuO8":
+        Script40();
+        break;
+      case "6grdR5czUeB":
+        Script41();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+};
